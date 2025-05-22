4b5c34faf9dd6dd9d2f995dcff543d025151f956
Resback <resback@gmail.com>
2025-05-22 02:40:26 -0600

    Add project files.

---
+++ ContextMenu.bat
@@ -0,0 +1,12 @@
+﻿@echo off
+ 
+
+:: Ruta donde está tu ejecutable
+set "RutaExe=N:\Herramientas\ConvertToJpgTool\ConvertToJpgTool.exe"
+
+:: Añadir clave en el registro
+reg add "HKEY_CLASSES_ROOT\*\shell\Convertir a JPG" /ve /t REG_SZ /d "Convertir a JPG" /f
+reg add "HKEY_CLASSES_ROOT\*\shell\Convertir a JPG\command" /ve /t REG_SZ /d "\"%RutaExe%\" \"%1\"" /f
+
+echo Opción 'Convertir a JPG' agregada al menú contextual con éxito.
+pause
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿
+using SkiaSharp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+         if (args.Length == 0)
+        {
+            Console.WriteLine("No se proporcionó ninguna imagen.");
+            return;
+        }
+
+        string[] formatosPermitidos = { ".png", ".jpg", ".jpeg", ".bmp", ".webp" };
+
+        foreach (string inputPath in args)
+        {
+            string extension = Path.GetExtension(inputPath).ToLower();
+            if (!formatosPermitidos.Contains(extension))
+            {
+                Console.WriteLine($"Formato no admitido: {inputPath}");
+                continue;
+            }
+
+            string outputPath = Path.ChangeExtension(inputPath, ".jpg");
+
+            try
+            {
+                using (SKBitmap bitmap = SKBitmap.Decode(inputPath))
+                using (SKImage image = SKImage.FromBitmap(bitmap))
+                using (SKData data = image.Encode(SKEncodedImageFormat.Jpeg, 85))
+                using (FileStream fs = File.OpenWrite(outputPath))
+                {
+                    data.SaveTo(fs);
+                }
+
+                Console.WriteLine($"Conversión exitosa: {outputPath}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error al convertir {inputPath}: {ex.Message}");
+            }
+
+         
+
+        }
+    }
+}
